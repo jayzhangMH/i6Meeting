339f8822d2be78318d3ce8b3b06fb987e1c2ba9d
jay <jay.zhang@meihua.info>
2014-06-03 15:30:19 +0800

    初始化

---
+++ Open.MingdaoSDK/Entity/ProjectModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace Open.MingdaoSDK.Entity
+{
+    [Serializable]
+    [XmlRoot("result")]
+    public class ProjectModel
+    {
+        private string _id;
+        [XmlElement("id")]
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _name;
+        [XmlElement("name")]
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _Error_Code;
+        [XmlElement("error_code")]
+        public string Error_Code
+        {
+            get { return _Error_Code; }
+            set { _Error_Code = value; }
+        }
+    }
+}
---
+++ Open.MingdaoSDK/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Open.MingdaoSDK;
+using Open.MingdaoSDK.Common;
+using Open.MingdaoSDK.Entity;
+using System.Collections.Specialized;
+
+namespace Open.MingdaoSDK
+{
+    public class Project : MDAPIBase
+    {
+        public Project(Parameter u_key) : base(u_key) { }
+
+        public ProjectModel GetProjectInfo()
+        {
+            string Result = base.SyncRequest(TypeOption.MD_Project, null, null);
+            if (!string.IsNullOrEmpty(Result))
+            {
+                return XmlSerializationHelper.XmlToObject<ProjectModel>(Result);
+            }
+            return null;
+        }
+    }
+}
